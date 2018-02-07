8d0ce02ef24a1959871d479d5e74d5d5260d6c5f
Jason Hughes <jasonhughes088@gmail.com>
2018-02-06 17:34:30 -0700

    Get and Post created for cameras in server file

--- server.js
+++ server.js
@@ -17,4 +17,49 @@ const requireHTTPS = (req, res, next) => {
     return res.redirect('https://' + req.get('host') + req.rul);
   }
   next();
-}
+};
+
+app.locals.title = 'BYOB';
+
+app.listen(app.get('port'), () => {
+
+})
+
+app.get('/', (request, response) => {
+  return response.send('curiosity')
+})
+
+
+app.get('/api/v1/cameras', (request, response) => {
+  database('cameras').select()
+    .then(projects => {
+      return response.status(200).json({ projects });
+    })
+    .catch(error => {
+      return response.status(500).json({ error })
+    })
+});
+
+app.post('/api/v1/cameras', (request, response) => {
+  const cameras = request.body;
+
+  for(let requiredParameter of ['name']) {
+    if(!cameras[requiredParameter]) {
+      return response.status(422).json({
+        error: `You are missing the required parameter ${requiredParameter}`
+      })
+    }
+  }
+  database('cameras').insert(cameras, 'id')
+    .then(cameras => {
+      return response.status(201).json({ id: cameras[0]})
+    })
+    .catch(error => {
+      return response.status(500).json({ error })
+    })
+})
+
+
+//get all photos
+
+//get all cameras
