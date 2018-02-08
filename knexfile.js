edff3153eabd36eafc685f3aeeca3e91a058876f
katiescruggs <katie.e.scruggs@gmail.com>
2018-02-08 13:02:12 -0700

    Finish setting up testing environment

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
     connection: 'postgres://localhost/curiosity_tests',
     useNullAsDefault: true,
     migrations: {
-      directory: __dirname + './db/migrations'
+      directory: __dirname + '/db/migrations'
     },
     seeds: {
       directory: './db/seeds/test'
